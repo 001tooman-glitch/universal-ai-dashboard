d8425c63703cb6cd8f08cba0e540ded2179552dd
001tooman-glitch <001tooman@gmail.com>
2026-07-29 16:16:43 +0500

    Update time_series.py

--- utils/time_series.py
+++ utils/time_series.py
@@ -1,1 +1,19 @@
+import pandas as pd
+
+def combine_tables(tables):
+
+    result = []
+
+    for table_name, df in tables.items():
+
+        temp_df = df.copy()
+
+        temp_df["Период"] = table_name
+
+        result.append(temp_df)
+
+    return pd.concat(
+        result,
+        ignore_index=True
+    )
 
