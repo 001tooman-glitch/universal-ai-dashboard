a84acd92da468ca60fd6ee46288709f83255eb67
001tooman-glitch <001tooman@gmail.com>
2026-07-29 15:28:30 +0500

    Update charts.py

--- utils/charts.py
+++ utils/charts.py
@@ -1,2 +1,24 @@
-# charts module
+import plotly.express as px
 
+def build_chart(df, metric, dimension):
+
+    chart_data = (
+        df.groupby(dimension)[metric]
+        .sum()
+        .reset_index()
+        .sort_values(
+            metric,
+            ascending=False
+        )
+        .head(15)
+    )
+
+    fig = px.bar(
+        chart_data,
+        x=dimension,
+        y=metric,
+        text=metric,
+        title=f"{metric} по {dimension}"
+    )
+
+    return fig
