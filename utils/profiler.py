dc7f0a99671b6980f6f60e56a1debe1a96010d7a
001tooman-glitch <001tooman@gmail.com>
2026-07-29 15:34:36 +0500

    Update profiler.py

--- utils/profiler.py
+++ utils/profiler.py
@@ -3,21 +3,8 @@ import pandas as pd
 def build_profile(df):
 
     return pd.DataFrame({
-
         "Поле": df.columns,
-
-        "Тип": [
-            str(df[c].dtype)
-            for c in df.columns
-        ],
-
-        "Пропуски": [
-            int(df[c].isna().sum())
-            for c in df.columns
-        ],
-
-        "Уникальных": [
-            int(df[c].nunique())
-            for c in df.columns
-        ]
+        "Тип": [str(df[c].dtype) for c in df.columns],
+        "Пропуски": [int(df[c].isna().sum()) for c in df.columns],
+        "Уникальных": [int(df[c].nunique()) for c in df.columns]
     })
