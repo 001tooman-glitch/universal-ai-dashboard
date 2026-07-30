4e5d52b4131b0e1f4c908b90f45a6138ad7f78da
001tooman-glitch <001tooman@gmail.com>
2026-07-30 11:33:26 +0500

    Update kpi_dashboard.py

--- utils/kpi_dashboard.py
+++ utils/kpi_dashboard.py
@@ -1,1 +1,112 @@
+import pandas as pd
 
+
+def build_dashboard_kpis(
+    df,
+    semantic_model
+):
+
+    result = {}
+
+    entities = semantic_model.get(
+        "entities",
+        {}
+    )
+
+    # =========================
+    # Общая стоимость
+    # =========================
+
+    if "amount" in entities:
+
+        try:
+
+            amount_col = (
+                entities["amount"][0]
+            )
+
+            result[
+                "Общая стоимость"
+            ] = round(
+
+                pd.to_numeric(
+                    df[amount_col],
+                    errors="coerce"
+                ).sum(),
+
+                2
+            )
+
+        except Exception:
+
+            pass
+
+    # =========================
+    # Количество объектов
+    # =========================
+
+    if "product" in entities:
+
+        try:
+
+            product_col = (
+                entities["product"][0]
+            )
+
+            result[
+                "Материалов"
+            ] = df[
+                product_col
+            ].nunique()
+
+        except Exception:
+
+            pass
+
+    # =========================
+    # Подразделения
+    # =========================
+
+    if "department" in entities:
+
+        try:
+
+            dep_col = (
+                entities["department"][0]
+            )
+
+            result[
+                "Подразделений"
+            ] = df[
+                dep_col
+            ].nunique()
+
+        except Exception:
+
+            pass
+
+    # =========================
+    # Периоды
+    # =========================
+
+    if "date" in entities:
+
+        try:
+
+            date_col = (
+                entities["date"][0]
+            )
+
+            result[
+                "Периодов"
+            ] = df[
+                date_col
+            ].nunique()
+
+        except Exception:
+
+            pass
+
+    result["Записей"] = len(df)
+
+    return result
