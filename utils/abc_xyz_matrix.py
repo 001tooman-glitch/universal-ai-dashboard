f6574fb1fbb2aa1c39542428324e8a8b9c60576b
001tooman-glitch <001tooman@gmail.com>
2026-07-30 10:22:15 +0500

    Update abc_xyz_matrix.py

--- utils/abc_xyz_matrix.py
+++ utils/abc_xyz_matrix.py
@@ -1,1 +1,39 @@
+import pandas as pd
 
+
+def build_abc_xyz_matrix(
+    abc_df,
+    xyz_df,
+    item_column
+):
+
+    matrix = pd.merge(
+        abc_df[
+            [
+                item_column,
+                "ABC"
+            ]
+        ],
+        xyz_df[
+            [
+                item_column,
+                "XYZ"
+            ]
+        ],
+        on=item_column,
+        how="inner"
+    )
+
+    matrix["ABC_XYZ"] = (
+        matrix["ABC"]
+        + matrix["XYZ"]
+    )
+
+    summary = (
+        matrix.groupby("ABC_XYZ")
+        .size()
+        .reset_index(name="Количество")
+        .sort_values("ABC_XYZ")
+    )
+
+    return matrix, summary
