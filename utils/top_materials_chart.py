e346bb5ec360b48ce6abc41a60eca67ba051a28d
001tooman-glitch <001tooman@gmail.com>
2026-07-30 11:45:00 +0500

    Update top_materials_chart.py

--- utils/top_materials_chart.py
+++ utils/top_materials_chart.py
@@ -1,1 +1,22 @@
+import plotly.express as px
 
+
+def build_top_materials_chart(
+    top_df,
+    product_column,
+    amount_column
+):
+
+    fig = px.bar(
+        top_df,
+        x=amount_column,
+        y=product_column,
+        orientation="h",
+        title="ТОП материалов по стоимости"
+    )
+
+    fig.update_layout(
+        height=700
+    )
+
+    return fig
