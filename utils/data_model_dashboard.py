c66c3d92ae8cc3dedd43ee9a084b3d2545c26391
001tooman-glitch <001tooman@gmail.com>
2026-07-30 10:44:21 +0500

    Update data_model_dashboard.py

--- utils/data_model_dashboard.py
+++ utils/data_model_dashboard.py
@@ -1,1 +1,61 @@
+from utils.semantic_engine import (
+    build_semantic_model
+)
 
+from utils.data_model_report import (
+    build_data_model_report
+)
+
+from utils.data_model_insights import (
+    generate_data_model_insights
+)
+
+from utils.data_model_visualizer import (
+    build_data_model_visualization
+)
+
+
+def build_data_model_dashboard(
+    df,
+    semantics
+):
+
+    semantic_model = (
+        build_semantic_model(
+            df,
+            semantics
+        )
+    )
+
+    report = (
+        build_data_model_report(
+            semantic_model
+        )
+    )
+
+    insights = (
+        generate_data_model_insights(
+            semantic_model
+        )
+    )
+
+    visualization = (
+        build_data_model_visualization(
+            semantic_model
+        )
+    )
+
+    return {
+
+        "semantic_model":
+            semantic_model,
+
+        "report":
+            report,
+
+        "insights":
+            insights,
+
+        "visualization":
+            visualization
+    }
