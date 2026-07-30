04f34b456b4b86449164415e7eca13ae4f805692
001tooman-glitch <001tooman@gmail.com>
2026-07-30 10:12:33 +0500

    Update abc_analyzer.py

--- utils/abc_analyzer.py
+++ utils/abc_analyzer.py
@@ -1,1 +1,45 @@
+import pandas as pd
 
+
+def run_abc_analysis(
+    df,
+    item_column,
+    value_column
+):
+
+    abc = (
+        df.groupby(item_column)[value_column]
+        .sum()
+        .reset_index()
+        .sort_values(
+            value_column,
+            ascending=False
+        )
+    )
+
+    total = abc[value_column].sum()
+
+    abc["Доля"] = (
+        abc[value_column] / total
+    )
+
+    abc["Накопленная доля"] = (
+        abc["Доля"].cumsum()
+    )
+
+    def classify(x):
+
+        if x <= 0.8:
+            return "A"
+
+        if x <= 0.95:
+            return "B"
+
+        return "C"
+
+    abc["ABC"] = (
+        abc["Накопленная доля"]
+        .apply(classify)
+    )
+
+    return abc
