69600c23b6f4568c0bd105e51a5a6a50227bf8e1
001tooman-glitch <001tooman@gmail.com>
2026-07-30 08:54:20 +0500

    Update kpi_detector.py

--- utils/kpi_detector.py
+++ utils/kpi_detector.py
@@ -1,1 +1,30 @@
+def detect_kpis(df):
 
+    kpis = []
+
+    for column in df.columns:
+
+        name = str(column).lower()
+
+        if any(x in name for x in [
+            "стоимость",
+            "сумма",
+            "затраты",
+            "выручка"
+        ]):
+            kpis.append(column)
+
+        elif any(x in name for x in [
+            "количество",
+            "остаток",
+            "объем"
+        ]):
+            kpis.append(column)
+
+        elif any(x in name for x in [
+            "план",
+            "факт"
+        ]):
+            kpis.append(column)
+
+    return list(set(kpis))
